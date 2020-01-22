1fb17791e0840c6798007747fe2ea0c448117884
Victor <victor@ananias.dev>
2020-01-21 22:08:40 -0300

    tags models added

---
+++ Tweetbook/Domain/Tag.cs
@@ -0,0 +1,18 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace Tweetbook.Domain
+{
+    public class Tag
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string CreatorId { get; set; }
+        [ForeignKey(nameof(CreatorId))]
+        public IdentityUser CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+}
